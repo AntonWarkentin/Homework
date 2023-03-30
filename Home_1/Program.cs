1649cb25c9c05ef40cf99609bfd50625ec1affa0
Anton Warkentin <reddoshoruda@gmail.com>
2023-03-30 20:55:17 +0500

    second commit

---
+++ Home_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write down your name!");
+            var yourName = Console.ReadLine();
+            Console.WriteLine("Hello {0}!", yourName);
+        }
+    }
+}
